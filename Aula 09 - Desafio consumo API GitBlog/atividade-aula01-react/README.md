ef53361eef3716a5a24469e0e76e41824426f687
rafael-dscarvalho <rafaeldscarvalho@hotmail.com>
2023-12-17 23:52:48 -0300

    enviada pasta da aula 09

---
+++ Aula 09 - Desafio consumo API GitBlog/atividade-aula01-react/README.md
@@ -0,0 +1,5 @@
+# REACT - Atividade 1
+
+Aula de React - Introdução
+
+<img src="https://i.imgur.com/VWZOL33.png">
---
+++ Aula 09 - Desafio consumo API GitBlog/atividade-aula01-react/src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Home } from './pages/Home/index.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Home />
+)
